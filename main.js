4814c0f2e09e281bb3423838c1f472463d816a53
Shuiiku <weiyu-zhou@somic.co.jp>
2022-09-15 08:56:18 +0900

    220915 8:56

--- main.js
+++ main.js
@@ -72,98 +72,99 @@ const graphCanvas = document.getElementById("graphCanvas"),
     drawGraph = document.getElementById("graphStart");
 
 let testData = [
-        [10, 100],
-        [50, 180],
-        [100, 50],
-        [150, 80],
-        [200, 20],
-        [250, 120],
-        [300, 180],
-        [350, 190],
-        [400, 40],
-        [450, 90],
-        [500, 70],
-        [550, 80],
-        [600, 150],
-        [650, 50],
-        [700, 30],
-        [750, 80],
-        [800, 150],
-        [850, 180],
-        [900, 70],
-        [950, 60],
-        [1000, 130],
-        [1050, 100],
-        [1100, 170],
-        [1150, 20],
-        [1200, 50],
-        [1250, 150]
-    ],
+    [0, 100],
+    [50, 180],
+    [100, 50],
+    [150, 80],
+    [200, 20],
+    [250, 120],
+    [300, 180],
+    [350, 190],
+    [400, 40],
+    [450, 90],
+    [500, 70],
+    [550, 80],
+    [600, 150],
+    [650, 50],
+    [700, 30],
+    [750, 80],
+    [800, 150],
+    [850, 180],
+    [900, 70],
+    [950, 60],
+    [1000, 130],
+    [1050, 100],
+    [1100, 170],
+    [1150, 20],
+    [1200, 50],
+    [1250, 150]
+],
     r = 5,
     sAngle = 0,
     eAngle = 2 * Math.PI,
-    i=0;
+    i = 0;
 
 drawGraph.addEventListener("click", () => {
 
-        graphCtx.clearRect(0, 0, graphCanvas.width, graphCanvas.height);
-        drawGraphCoordinate();
-        let timer = setInterval(() =>{
-            if (i>(testData.length-2)){
-                clearInterval(timer);
-                i=0;
-            }else{
-                drawGraphPoint();
-                drawGraphLine();
-                i++;
-            }
-        },500)
+    graphCtx.clearRect(0, 0, graphCanvas.width, graphCanvas.height);
+    drawGraphCoordinate();
+    let timer = setInterval(() => {
+        if (i > (testData.length - 2)) {
+            clearInterval(timer);
+            i = 0;
+        } else {
+            drawGraphPoint();
+            drawGraphLine();
+            i++;
+        }
+    }, 500)
 
 });
 
-function  drawGraphPoint(){
+function drawGraphPoint() {
     graphCtx.lineWidth = 1.5;
     graphCtx.strokeStyle = 'blue';
     graphCtx.beginPath();
     graphCtx.arc(
-        testData[i+1][0],
-        testData[i+1][1],
+        testData[i + 1][0],
+        testData[i + 1][1],
         r,
         sAngle,
         eAngle,
         true);
     graphCtx.fillStyle = 'blue';
-        graphCtx.fill();
+    graphCtx.fill();
     graphCtx.stroke();
     graphCtx.closePath();
 }
 
-function  drawGraphLine() {
+function drawGraphLine() {
     graphCtx.lineWidth = 1.5;
     graphCtx.strokeStyle = 'green';
     graphCtx.beginPath();
     graphCtx.moveTo(testData[i][0], testData[i][1]);
-    graphCtx.lineTo(testData[i+1][0], testData[i+1][1]);
+    graphCtx.lineTo(testData[i + 1][0], testData[i + 1][1]);
     graphCtx.stroke();
     graphCtx.closePath();
 }
 
-function  drawGraphCoordinate() {
+function drawGraphCoordinate() {
     graphCtx.lineWidth = 1.5;
     graphCtx.strokeStyle = 'black';
     graphCtx.beginPath();
-    graphCtx.moveTo(10,190);
-    graphCtx.lineTo(10,25);
-    graphCtx.moveTo(10,10);
-    graphCtx.lineTo(5,25);
-    graphCtx.lineTo(15,25);
-    graphCtx.lineTo(10,10);
-    graphCtx.moveTo(0,110);
-    graphCtx.lineTo(1275,110);
-    graphCtx.moveTo(1290,110);
-    graphCtx.lineTo(1275,105);
-    graphCtx.lineTo(1275,115);
-    graphCtx.lineTo(1290,110);
+    graphCtx.moveTo(15, 190);
+    graphCtx.lineTo(15, 25);  // y axis
+    graphCtx.moveTo(15, 10);
+    graphCtx.lineTo(10, 25);
+    graphCtx.lineTo(20, 25);
+    graphCtx.lineTo(15, 10);
+    graphCtx.moveTo(0, 110);
+    graphCtx.lineTo(1275, 110); // x axis
+    graphCtx.moveTo(1290, 110);
+    graphCtx.lineTo(1275, 105);
+    graphCtx.lineTo(1275, 115);
+    graphCtx.lineTo(1290, 110);
     graphCtx.stroke();
     graphCtx.closePath();
+    graphCtx.strokeRect(10, 105, 10, 10);
 }
